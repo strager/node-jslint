91072659f0e042f85b1827aad9041f7a9878dc9f
Curtis Harvey <curtis@curtisharvey.com>
2010-09-14 18:19:19 -0400

    more robust fulljslint.js update script

--- lib/update.sh
+++ lib/update.sh
@@ -4,6 +4,18 @@
 # if you want to review changes in increments see:
 # http://github.com/happygiraffe/jslint.com-mirror/commits/master/www.jslint.com/fulljslint.js
 
-cd `dirname $0` &&
-    curl -s jslint.com/fulljslint.js > fulljslint_export.js &&
-    echo 'exports.JSLINT = JSLINT;' >> fulljslint_export.js
+input="http://jslint.com/fulljslint.js"
+output="`dirname $0`/fulljslint_export.js"
+
+# use wget if available, curl if not
+if type -t wget > /dev/null; then
+    fetch_cmd="wget -O"
+elif type -t curl > /dev/null; then
+    fetch_cmd="curl -o"
+else
+    echo "wget or curl must be installed to fetch '$input'";
+    exit 1;
+fi
+
+$fetch_cmd "$output" "$input" &&
+    echo "\n// export for node-jslint\nexports.JSLINT = JSLINT;" >> "$output"
